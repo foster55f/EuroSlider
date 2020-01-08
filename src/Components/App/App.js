28cae753d3cb8580e6f21fd03c64c30208f033d1
“Foster <fosterataylor@yahoo.com>
2020-01-08 12:10:12 -0700

    Add HighlightContainer component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import HighlightContainer from '../HighlightContainer/HighlightContainer'
 
 class App extends React.Component {
   constructor() {
@@ -14,7 +15,9 @@ class App extends React.Component {
 
   render() {
     return (
-      <></>
+      <>
+        <HighlightContainer />
+      </>
     )
   }
 }
---
+++ src/Components/HighlightContainer/HighlightContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HighLightContainer = () => {
+    return (
+        <></>
+    )
+}
+
+export default HighLightContainer
