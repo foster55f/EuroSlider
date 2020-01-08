df223dc54e045ef8317fb25e07f9864560ca9ab3
“Foster <fosterataylor@yahoo.com>
2020-01-08 14:52:40 -0700

    Add HighlightCard structure

--- src/Components/HighlightContainer/HighlightContainer.js
+++ src/Components/HighlightContainer/HighlightContainer.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import HighlightCard from '../HighlightCard/HighlightCard'
 
-const HighLightContainer = () => {
+const HighLightContainer = ({ games }) => {
     return (
-        <></>
+        <div className= 'game-container'>
+        {games.map(game => {
+            return (
+            <HighlightCard
+                key={game.id}
+                id={game.id}
+            />
+            )
+        })}
+        </div>
     )
 }
 
