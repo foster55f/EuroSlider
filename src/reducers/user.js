7b9945e40eb6ed2a444e5e80129623d36bb0a622
“Foster <fosterataylor@yahoo.com>
2020-01-11 09:41:24 -0700

    Add user reducer

---
+++ src/reducers/user.js
@@ -0,0 +1,8 @@
+export const user = (state = {}, action) => {
+    switch (action.type) {
+        case "ADD_USER":
+            return {...state, ...action.user}
+        default:
+            return state
+    }
+}
