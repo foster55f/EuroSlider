d7578735d1dba68a14dcd051b1f082428499c5d8
“Foster <fosterataylor@yahoo.com>
2020-01-11 09:33:01 -0700

    Refactor and start initial structure for user login

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -18,7 +18,7 @@ class Login extends React.Component {
         if (this.state.username === '' || this.state.passwordLength > 0) {
             this.setState({ error: 'THE USERNAME OR PASSWORD IS INCORECT' })
           }
-        localStorage.setItem('key', this.state.username)
+        localStorage.setItem(this.state.username, 'key1')
     }
 
     render() {
@@ -28,10 +28,12 @@ class Login extends React.Component {
                 <nav>
                 <h2 className='header-title'>Euro Slider</h2>
                             <>
-                            <input className = 'userName'type="text" placeholder='ENTER NAME' onChange={(event) => this.setState({ username: event.target.value })} />
+                            <input className='userName' type="text" placeholder='ENTER NAME' onChange={(event) => this.setState({ username: event.target.value })} />
+                            <input className = 'password'type="text" placeholder='password' onChange={(event) => this.setState({ username: event.target.value })} />
                             {/* <input type="password" placeholder='PASSWORD' onChange={(event) => { this.setState({ password: event.target.value }); this.setState({ passwordLength: event.target.value.length }); }} />  */}
-                            <h1>Hello {this.state.username}</h1>
+                            {/* <h1>Hello {this.state.username}</h1> */}
                             {this.state.error}
+                            <button className = 'favorite-button'onClick={this.goToFavorites}> Click to Creat Your Profile</button> 
                             <button className = 'favorite-button'onClick={this.goToFavorites}> Click to View Your Favorite Highlights!</button> 
                             </>
                 </nav>
