355fa1205f5a822524dc28c8f8d8485b691ec911
“Foster <fosterataylor@yahoo.com>
2020-01-12 14:07:04 -0700

    Add more action tests

--- src/actions/highlightActions.test.js
+++ src/actions/highlightActions.test.js
@@ -39,5 +39,95 @@ describe('actions', () => {
         const result = actions.addFavoriteGames(favoriteGames)
 
         expect(result).toEqual(expectedAction);
-    })
+    }),
+
+    it('should have a type of REMOVE_GAMES', () => {
+        // Setup
+        const favoriteGames = ['barcelona'];
+        const expectedAction = {
+            type: 'REMOVE_GAMES',
+            favoriteGames:['barcelona']
+        }
+
+        // Execution
+        const result = actions.removeGames(favoriteGames);
+
+        // Expectation
+        expect(result).toEqual(expectedAction);
+    });
+
+    it('should have a type of ADD_USER', () => {
+        // Setup
+        const user = {name:'foster'};
+        const expectedAction = {
+            type: 'ADD_USER',
+            user:{name:'foster'}
+        }
+
+        // Execution
+        const result = actions.addUser(user);
+
+        // Expectation
+        expect(result).toEqual(expectedAction);
+    });
+
+    it('should have a type of LOGIN', () => {
+        // Setup
+        const userLog = true;
+        const expectedAction = {
+            type: 'LOGIN',
+            userLog:true
+        }
+
+        // Execution
+        const result = actions.userLogIn(userLog);
+
+        // Expectation
+        expect(result).toEqual(expectedAction);
+    });
+
+    it('should have a type of LOGOUT', () => {
+        // Setup
+        const userLog = false;
+        const expectedAction = {
+            type: 'LOGOUT',
+            userLog:false
+        }
+
+        // Execution
+        const result = actions.userLogOut(userLog);
+
+        // Expectation
+        expect(result).toEqual(expectedAction);
+    });
+
+    it('should have a type of USER_FAVORITE_GAMES', () => {
+        // Setup
+        const userFavoriteGames = ['barcelona'];
+        const expectedAction = {
+            type: 'USER_FAVORITE_GAMES',
+            userFavoriteGames:['barcelona']
+        }
+
+        // Execution
+        const result = actions.addUserFavoriteGames(userFavoriteGames);
+
+        // Expectation
+        expect(result).toEqual(expectedAction);
+    });
+
+    it('should have a type of REMOVE_USER_GAMES', () => {
+        // Setup
+        const userFavoriteGames = [];
+        const expectedAction = {
+            type: 'REMOVE_USER_GAMES',
+            userFavoriteGames:[]
+        }
+
+        // Execution
+        const result = actions.removeUserGames(userFavoriteGames);
+
+        // Expectation
+        expect(result).toEqual(expectedAction);
+    });
 })
